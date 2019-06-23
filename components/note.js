5a68eed66d1d5b490bf37950e2d6a652c1f20c47
Christopher Pitt <cgpitt@gmail.com>
2019-06-23 09:59:46 +0200

    Fix some display issues

--- components/note.js
+++ components/note.js
@@ -2,14 +2,14 @@ import PropTypes from "prop-types"
 import { superToNormal } from "../helpers"
 
 const Note = ({ number, text }) => {
-    const html = text.replace(/[\u0590-\u05FF]+/g, match => `<span class="hebrew">${match}</span>`)
+    const html = text.replace(/[\u0590-\u05FF]+/g, match => `<span class="hebrew text-base">${match}</span>`)
 
     return (
         <div className="mb-2 text-sm">
             <a
                 href={`#note-${superToNormal(number)}`}
                 id={`note-description-${superToNormal(number)}`}
-                className="mr-1 focus:outline-none focus:bg-green-500 focus:text-white"
+                className="mr-1 focus:outline-none focus:bg-green-500 focus:text-white p-1"
             >
                 {number}
             </a>
--- components/verse.js
+++ components/verse.js
@@ -1,21 +1,28 @@
 import PropTypes from "prop-types"
 import { superToNormal } from "../helpers"
+import he from "he"
 
 const Verse = ({ number, text }) => {
-    let html = text
+    let html = he
+        .decode(text)
+        .replace('"content"', '"content" dir="ltr"')
         .replace(
             /([⁰¹²³⁴⁵⁶⁷⁸⁹]+)/g,
-            "<a href='#note-description-$1' class='no-underline focus:outline-none focus:bg-green-500 focus:text-white' id='note-$1'>$1</a>",
+            "<a dir='ltr' href='#note-description-$1' class='relative inline no-underline focus:outline-none focus:bg-green-500 focus:text-white text-lg p-1' id='note-$1'>$1</a>",
         )
-        .replace(/[\u0590-\u05FF]+/g, match => `<span class="hebrew">${match}</span> `)
+        .replace(
+            /[\u0590-\u05FF]+/g,
+            match =>
+                `</span>&nbsp;<span class="hebrew text-xl" dir="rtl">${match}</span>&nbsp;<span class="content" dir="ltr">`,
+        )
 
     html = superToNormal(html, "note-description-")
     html = superToNormal(html, "note-")
 
     return (
-        <div className="mb-2">
+        <div className="mb-2 verse">
             <span className="text-xs mr-1 text-gray-600">{number}</span>
-            <span dangerouslySetInnerHTML={{ __html: html }} />
+            <span dir="ltr" dangerouslySetInnerHTML={{ __html: html }} />
         </div>
     )
 }
